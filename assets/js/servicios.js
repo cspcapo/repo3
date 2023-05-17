596fc1183d1ea3a2587a1d6e0a03388c3247bbc0
cspcapo <117932584+cspcapo@users.noreply.github.com>
2023-05-17 15:40:18 -0300

    Add files via upload

---
+++ assets/js/servicios.js
@@ -0,0 +1,23 @@
+
+function mostrarPromo1(){
+    
+
+    document.getElementById("txt-promo").innerHTML ="¿Te has imaginado recorriendo el mundo y conociendo culturas diferentes? ¡Hazlo realidad con nuestra agencia de viajes! Tenemos los mejores paquetes turísticos para que puedas recorrer los lugares más fascinantes del mundo. ¡No te pierdas esta aventura!" ;
+
+}
+
+function mostrarPromo2(){
+    document.getElementById("txt-promo").innerHTML ="¡Vive experiencias únicas e inolvidables con nuestra agencia de viajes! Te ofrecemos paquetes turísticos personalizados para que puedas disfrutar de lo que más te gusta. ¿Quieres vivir una aventura en la selva o relajarte en una playa paradisíaca? ¡Lo que quieras, lo tenemos para ti!";
+
+}
+
+function mostrarPromo3(){
+    document.getElementById("txt-promo").innerHTML = "¿Quieres viajar pero no quieres gastar una fortuna en vuelos? ¡No hay problema! En nuestra agencia de viajes te ofrecemos los vuelos más baratos del mercado. ¡Viaja a donde quieras al mejor precio!"  ;
+
+}
+
+function mostrarPromo4(){
+    document.getElementById("txt-promo").innerHTML = '¡Escápate de la rutina y descubre nuevos lugares con nuestra agencia de viajes! Tenemos las mejores ofertas en escapadas cortas para que puedas relajarte y disfrutar de paisajes increíbles. ¡No pierdas la oportunidad de vivir nuevas experiencias! ';
+
+}
+
